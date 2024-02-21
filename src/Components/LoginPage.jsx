148bd8bb88ef3cad088c00bdda97318455176a8b
vanshmathur2003 <vanshmathur26@gmail.com>
2024-02-21 11:51:05 +0530

    ok

--- src/Components/LoginPage.jsx
+++ src/Components/LoginPage.jsx
@@ -1,4 +1,6 @@
 import { Link } from "react-router-dom"
+import { LoginSignupValidate } from "../Utils/LoginSignupValidate"
+import { useRef,useState } from "react"
 
 
 
@@ -7,6 +9,16 @@ const bgImg = "https://assets.nflxext.com/ffe/siteui/vlv3/c0b69670-89a3-48ca-877
 
 
 const LoginPage = () => {
+  const [errorMessage,setErrorMessage] = useState(null)
+  const pass = useRef()
+  const email = useRef()
+
+  const handleBtnClick = () =>{
+  const message = LoginSignupValidate(email.current.value,pass.current.value)
+  setErrorMessage(message)
+  }
+
+
   return (
     <>
       <img className="h-screen absolute " src={bgImg} alt="bgimg" />
@@ -15,10 +27,11 @@ const LoginPage = () => {
       <div className="absolute text-white  bg-black bg-opacity-60 w-3/12 mx-auto left-0 right-0 mt-[250px] rounded-sm ">
         <div className="p-12">
         <h1 className="text-3xl pl-2 pb-2">Sign In</h1>
-        <form className="flex flex-col ">
-          <input type="text" placeholder="Email Address" className="p-2 pl-4 m-2 rounded-md bg-gray-700" />
-          <input type="password" placeholder="Password"  className="p-2 pl-4 m-2 rounded-md  bg-gray-700"/>
-          <button  className="p-2 m-2 bg-red-700 rounded-md ">Sign In</button>
+        <form className="flex flex-col " onSubmit={(e)=>{e.preventDefault()}}>
+          <input ref={email} type="text" placeholder="Email Address" className="p-2 pl-4 m-2 rounded-md bg-gray-700" />
+          <input ref={pass} type="password" placeholder="Password"  className="p-2 pl-4 m-2 rounded-md  bg-gray-700"/>
+          <p className="ml-4 text-red-500 text-[10px]">{errorMessage}</p>
+          <button  onClick={handleBtnClick} className="p-2 m-2 bg-red-700 rounded-md ">Sign In</button>
         </form>
         <div className="flex flex-row mt-12 ml-2">
           <h3 className="pr-2 text-gray-400">New to Netflix? </h3>
---
+++ src/Utils/LoginSignupValidate.js
@@ -0,0 +1,15 @@
+
+export const LoginSignupValidate = (email,password) => {
+    const isEmailValid = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/.test(email)
+    const isPasswordValid = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,}$/.test(password)
+
+    if (!isEmailValid){
+        return "Please enter a valid email"
+    }else if (!isPasswordValid){
+        return "Password must contain at least one uppercase letter, one lowercase letter, one number, and be at least 8 characters long."
+    }else{
+        return null
+    }
+ 
+}
+
