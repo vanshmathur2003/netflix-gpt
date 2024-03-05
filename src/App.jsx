b5e872e0b03723b0083b862c8e372b73125887c9
vanshmathur2003 <vanshmathur26@gmail.com>
2024-03-05 19:50:57 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -32,6 +32,8 @@ function App() {
         // User is signed in, see docs for a list of available properties
         const { uid, email, displayName } = user;
         dispatch(addUser({ uid: uid, email: email, displayName: displayName }))
+        console.log("ok")
+        // navigate("/Browse")
         // ...
       } else {
         // User is signed out
